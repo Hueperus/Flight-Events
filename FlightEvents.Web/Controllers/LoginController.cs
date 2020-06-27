e2a64bf3d81ebd90921a10020c85dc764fd7b433
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-06-27 15:00:25 +0800

    Add login by MS account

---
+++ FlightEvents.Web/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using HotChocolate.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FlightEvents.Web.Controllers
+{
+    public class LoginController : ControllerBase
+    {
+        [Route("Login/Microsoft")]
+        public IActionResult Microsoft([FromQuery] string ReturnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+                return Redirect("~/");
+
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = ReturnUrl
+            }, "Microsoft");
+        }
+
+        [Route("Logout")]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("Microsoft");
+        }
+    }
+}
